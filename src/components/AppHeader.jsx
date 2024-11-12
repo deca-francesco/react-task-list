63de0f97e7fe1da448ec62143240ac35a8f64a77
deca-francesco <francescodecarlo70@gmail.com>
2024-11-12 10:17:32 +0100

    UPDATE: lesson correction

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,6 +2,8 @@ export default function AppHeader() {
 
     const title = "Task Manager";
 
+    // possiamo anche fare il return della variabile header
+
     return (
         <header>
             <h1>{title}</h1>
